173274390d16201aefa5173fc0b8349310836546
battlesushi <105021030@live.asia.edu.tw>
2016-12-05 20:23:08 +0800

    ex

--- src/ex/ex03.java
+++ src/ex/ex03.java
@@ -1,7 +1,6 @@
 ﻿package ex;
 import java.util.ArrayList;
 import java.util.Scanner;
-
 /*
  * Topic: 請設計 Person 類別 (姓名,性別,身高,體重)，使用者可以新增個人資料，
  * 每新增一筆就會列出目前的資料串有哪些人，而這些資料請用插入排序法依每一個人的 BMI 值由大到小排序。
