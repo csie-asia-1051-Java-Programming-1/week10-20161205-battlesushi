aa71cc1993c2d6e3b9517b3590caefe73136c96a
battlesushi <105021030@live.asia.edu.tw>
2016-12-05 14:27:16 +0800

    ex

--- src/ex/ex02.java
+++ src/ex/ex02.java
@@ -1,15 +1,35 @@
 ﻿package ex;
+import java.util.Scanner;
 /*
  * Topic: 輸入所使用的度數，換算夏月及非夏月之電費金額。
  * Date: 2016/12/05
- * Author: 1050210XX 周永振老師
+ * Author: 105021030 張廷毓
  */
-
 public class ex02 {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc=new Scanner(System.in);
+		float a=sc.nextFloat();
+		if(a<=120){
+			System.out.print(a*2.10f);
+			System.out.print(a*2.10f);
+		}
+		else if(a>120&&a<=330){
+			System.out.print(a*3.02f);
+			System.out.print(a*2.68f);
+		}
+		else if(a>330&&a<=500){
+			System.out.print(a*4.39f);
+			System.out.print(a*3.61f);
+		}
+		else if(a>500&&a<=700){
+			System.out.print(a*4.97f);
+			System.out.print(a*4.01f);
+		}
+		else{
+			System.out.print(a*5.63f);
+			System.out.print(a*4.50f);
+		}
+		
 	}
 
 }
