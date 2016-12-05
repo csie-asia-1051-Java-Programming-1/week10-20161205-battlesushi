38c037cb7a1df0f55abe9dbfada20361e5177815
battlesushi <105021030@live.asia.edu.tw>
2016-12-05 15:58:57 +0800

    ex

--- src/ex/ex02.java
+++ src/ex/ex02.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
  * Date: 2016/12/05
  * Author: 105021030 張廷毓
  */
-public class ex02 {
+public class ex02 {//部分 乘上其電費金額
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
 		float a=sc.nextFloat();
@@ -14,20 +14,22 @@ public class ex02 {
 			System.out.println(a*2.10f);
 		}
 		else if(a>120&&a<=330){
-			System.out.println(a*3.02f);
-			System.out.println(a*2.68f);
+			System.out.println(120*2.10f+(a-120)*3.02f);
+			System.out.println(120*2.10f+(a-120)*2.68f);
 		}
 		else if(a>330&&a<=500){
-			System.out.println(a*4.39f);
-			System.out.println(a*3.61f);
+			System.out.println(120*2.10f+210*3.02f+(a-330)*4.39f);
+			System.out.println(120*2.10f+210*2.68f+(a-330)*3.61f);
 		}
 		else if(a>500&&a<=700){
-			System.out.println(a*4.97f);
-			System.out.println(a*4.01f);
+			System.out.println(120*2.10f+210*3.02f+170*4.39f+(a-500)*4.97f);
+			System.out.println(120*2.10f+210*2.68f+170*3.61f+(a-500)*4.01f);
 		}
 		else{
-			System.out.println(a*5.63f);
-			System.out.println(a*4.50f);
+			System.out.println();
+			System.out.println();
+			System.out.println(120*2.10f+210*3.02f+170*4.39f+200*4.97f+(a-700)*5.63f);
+			System.out.println(120*2.10f+210*2.68f+170*3.61f+200*4.01f+(a-700)*4.50f);
 		}
 		
 	}
--- src/ex/ex03.java
+++ src/ex/ex03.java
@@ -1,14 +1,22 @@
 ﻿package ex;
+import java.util.ArrayList;
 import java.util.Scanner;
+
 /*
- * Topic: 請設計 Person 類別 (姓名,性別,身高,體重)，使用者可以新增個人資料，每新增一筆就會列出目前的資料串有哪些人，而這些資料請用插入排序法依每一個人的 BMI 值由大到小排序。
+ * Topic: 請設計 Person 類別 (姓名,性別,身高,體重)，使用者可以新增個人資料，
+ * 每新增一筆就會列出目前的資料串有哪些人，而這些資料請用插入排序法依每一個人的 BMI 值由大到小排序。
  * Date: 2016/12/05
  * Author: 105021030 張廷毓
  */
-public class ex03 {
+public class ex03 {							//W/H^2
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
-
+		ArrayList<Person> list=new ArrayList<Person>();
 	}
+}
 
+class Person{
+private String name;
+private String sex;
+private float height,weight;
 }
